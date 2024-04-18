57bf60d8ba9e7b0ccadd4ffce1e7d4593b6e16d1
Jennifer McNiel <jennifer.mcniel@yahoo.com>
2024-04-18 14:54:14 -0400

    Added order summary and img

--- index.php
+++ index.php
@@ -16,19 +16,45 @@ $f3 = Base::instance();
 //https://jmcniel.greenriverdev.com/328/hello-fat-free/
 $f3->route('GET /', function(){
 //    echo below is used for testing before executing the template
-    echo '<h1>Hello Pets</h1>';
+        echo '<h1>Hello Pets</h1>';
 
 //    //Render a view page
     $view = new Template();
     echo $view->render('view/home.html');
 });
 
-$f3->route('GET /order', function() {
-    echo '<h1>Order Page</h1>';
+$f3->route('GET|POST /order', function($f3) {
+
+    if($_SERVER['REQUEST_METHOD']== 'POST') {
+        $pet = $_POST['pet'];
+        $color = $_POST['color'];
+
+        if(empty($pet)){
+            echo "Please supply a pet type";
+        }else {
+            $f3->set('SESSION.pet', $pet);
+            $f3->set('SESSION.color', $color);
+
+            $f3->reroute('summary');
+        }
+    }
+
+
+//    echo '<h1>Order Page</h1>';
 
     // Render a view page
     $view = new Template();
     echo $view->render('view/pet-order.html');
+});
+
+//Order Summary
+$f3->route('GET /summary', function($f3) {
+
+//    var_dump( $f3->get('SESSION'));
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('view/order-summary.html');
 });
 
 //Run Fat-Free
